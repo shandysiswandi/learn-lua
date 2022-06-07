518785cbe7c5c53c68ebe738d3a9eb6a7fe210d5
Shandy Siswandi <shandysiswandi@gmail.com>
2022-06-07 20:46:47 +0700

    feat: add comment

---
+++ src/1_comment.lua
@@ -0,0 +1,18 @@
+-- This is a single line comment in lua
+
+local comment = "comment" -- This is a comment after the code
+
+--[[
+    This is a multi line comments in lua.
+    You can wrire anything inside here to add notes in you code.
+]]
+
+--[[
+    ALL RESERVED KEYWORDS
+
+    - and       - break     - do        - else
+    - elseif    - end       - false     - for
+    - function  - if        - in        - local
+    - nil       - not       - or        - repeat
+    - return    - then      - true      - until - while
+]]
