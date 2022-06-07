6aae1dc50932ad60b5b7847940ddb98b6286c40b
Shandy Siswandi <shandysiswandi@gmail.com>
2022-06-07 20:47:14 +0700

    feat: add printing

---
+++ src/2_print.lua
@@ -0,0 +1,7 @@
+-- io.write is same ad print, the difference is they does not add new line in last arguments
+
+io.write("Hello Lua! \n")
+
+-- print is use to print arguments to stdout with add new line
+-- if you add argumnet by `,` you will get the output like +TAB
+print("hello world")
